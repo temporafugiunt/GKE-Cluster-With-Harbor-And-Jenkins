9f44eee50b03f40f7b8ef1706883f4b6ea342051
James Eby <temporafugiunt@gmail.com>
2019-06-17 11:34:13 -0500

    Updated so that harbor registry properly interacts with a Istio Gateway and VirtualService for LetsEncrypt support rather than Kubernetes Ingress.

--- cloud-ops/kubernetes/devops-sandbox-cluster/04-install-devops-pipeline-apps.sh
+++ cloud-ops/kubernetes/devops-sandbox-cluster/04-install-devops-pipeline-apps.sh
@@ -8,7 +8,7 @@
 #
 
 # Set the certificate issuer used for applications installed below
-export CERT_ISSUER=letsencrypt-staging-issuer
+export CERT_ISSUER=letsencrypt-production-issuer
 
 # Install Jenkins instance and all the Istio specific objects not supported by Helm chart to expose Jenkins properly
 # to the outside world not using Ingress.
@@ -45,23 +45,30 @@ export HOST_PORT=5000
 export OBJECT_BASE_NAME=sandboxregistry-ddns
 export DOMAIN_NAME=sandboxregistry.ddns.net
 . $COMMON_BASH_FILES_PATH/setup-cert-and-gateway-for-dns-name.sh
+
 # # Create a network tester for testing the internal network of the cluster.
 # kubectl create -f $COMMON_FILES_PATH/Deployment/busyBoxTester.yaml
 
-# # Setup harbor
-# git clone https://github.com/goharbor/harbor-helm harbor-helm
+# Setup harbor
+git clone https://github.com/goharbor/harbor-helm harbor-helm
 
-# cd ./harbor-helm
+cd ./harbor-helm
 
-# # Commits from 11-21-2018 broke persistence, need to investigate values.yaml changes in future.
-# git checkout -b 1.0.0 origin/1.0.0
+# Commits from 11-21-2018 broke persistence, need to investigate values.yaml changes in future.
+git checkout -b 1.0.0 origin/1.0.0
 
-# helm dependency update
-# helm upgrade --install --namespace build harbor -f $CLUSTER_FILES_PATH/Deployment/harbor-helm-values.yaml --set harborAdminPassword=$HARBOR_ADMIN_USER_PASSWORD --set secretKey=$HARBOR_SECRET_KEY --set database.internal.password=$HARBOR_ADMIN_USER_PASSWORD .
+helm dependency update
+helm upgrade --install --namespace build harbor -f $CLUSTER_FILES_PATH/Deployment/harbor-helm-values.yaml --set harborAdminPassword=$HARBOR_ADMIN_USER_PASSWORD --set secretKey=$HARBOR_SECRET_KEY --set database.internal.password=$HARBOR_ADMIN_USER_PASSWORD .
 # # kubectl exec -it harbor-harbor-database-0 -n build usr/bin/bash
 # #   psql -U postgres
 # #   \l
 # #   \dl
+# Setup the gateways tying the last cert to helloworld service
+export HOST_NAMESPACE=build
+export HOST_NAME=harbor
+export HOST_PORT=80
+. $COMMON_BASH_FILES_PATH/tie-host-to-gateway-via-vservice.sh
+
 
 # echo 'Sleeping for 30 seconds'
 # sleep 30s
