4518c920f8b109a48d2607cc7b23fc6ba380dadd
James Eby <temporafugiunt@gmail.com>
2019-07-11 12:33:00 -0500

    Removed unused app from this demo as apps are external.

--- src/ClientApp/app.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    url: 'https://dev-541900.okta.com',
-    issuer: 'https://dev-541900.okta.com/oauth2/default',
-    redirect_uri: window.location.origin + '/implicit/callback',
-    client_id: '0oacjv8lvj9LMZ4kX356'
-  }
--- src/Models/RegistrationResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace okta_dotnetcore_react_example.Models
-{
-    public class RegistrationResponse
-    {
-        public string Message { get; set; }
-    }
-}
