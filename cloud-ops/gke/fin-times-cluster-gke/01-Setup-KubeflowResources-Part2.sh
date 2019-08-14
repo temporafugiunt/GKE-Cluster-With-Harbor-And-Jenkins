f54f56473c6524f7be47139fef89fa19668f11b0
MW Loaner 366 <mwloaner366@ITs-MacBook-Pro.local>
2019-08-14 10:56:40 -0500

    Made changes requested by fin-times sample, also verified n1-highmem-4 can be used in the cpu-pool-machine-type.

--- cloud-ops/gke/fin-times-cluster-gke/01-Setup-KubeflowResources-Part2.sh
+++ cloud-ops/gke/fin-times-cluster-gke/01-Setup-KubeflowResources-Part2.sh
@@ -2,8 +2,10 @@
 # This bash script kubeflow cluster creation after manual update of YAML and jinja files based upon:
 # https://github.com/kubeflow/examples/tree/master/financial_time_series
 
+cd fin-times-cluster
+
 kfctl apply all -V
 
 echo "You should be able to access https://${KFAPP}.endpoints.${PROJECT}.cloud.goog/ within 20 minutes"
 
-cd ../../../cluster-ops/kubernetes/fin-times-cluster-gke/
+# cd ../../../cluster-ops/kubernetes/fin-times-cluster-gke/
